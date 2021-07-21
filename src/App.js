67aad0c6b9c68d51563751ac31d636f1a2540455
Abdelrahman El-Adawy <abdelrahman.eladawy@robustastudio.com>
2021-07-21 15:45:44 +0200

    styles: basic global light/dark theme styles using styled-components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { ThemeProvider } from 'styled-components';
+import {
+  lightTheme,
+  darkTheme,
+  GlobalStyles,
+  GlobalAppWrapper
+} from './themes';
 
 function App() {
+  const [theme, updateTheme] = useState(darkTheme);
+  const themeToggle = () => {
+    theme === lightTheme && updateTheme(darkTheme);
+    theme === darkTheme && updateTheme(lightTheme);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <GlobalAppWrapper>
+        <div className='div'>Hi</div>
+      </GlobalAppWrapper>
+    </ThemeProvider>
   );
 }
 
---
+++ src/themes.js
@@ -0,0 +1,24 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+const lightTheme = {
+  body: '#fff',
+  fontColor: '#000'
+};
+
+const darkTheme = {
+  body: '#000',
+  fontColor: '#fff'
+};
+
+const GlobalStyles = createGlobalStyle`
+body {
+    background-color:${props => props.theme.body}
+}`;
+
+const GlobalAppWrapper = styled.div`
+  .div {
+    color: ${props => props.theme.fontColor};
+  }
+`;
+
+export { lightTheme, darkTheme, GlobalStyles, GlobalAppWrapper };
