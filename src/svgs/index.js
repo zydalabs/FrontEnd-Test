ed167efd8e3fb1f0f98e2e4c39515e2c853701ef
Abdelrahman El-Adawy <abdelrahman.eladawy@robustastudio.com>
2021-07-26 02:04:17 +0200

    styles: changing svgs

--- src/svgs/index.js
+++ src/svgs/index.js
@@ -1,4 +1,6 @@
 export { default as sun } from './sun.svg';
 export { default as moon } from './moon.svg';
-export { default as darkThemeArrow } from './dark-back-arrow.svg';
-export { default as lightThemeArrow } from './light-back-arrow.svg';
+export { default as darkThemeArrow } from './back-arrow-dark.svg';
+export { default as lightThemeArrow } from './back-arrow-light.svg';
+export { default as darkThemeSearch } from './search-dark.svg';
+export { default as lightThemeSearch } from './search-light.svg';
