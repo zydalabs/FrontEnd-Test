375cb91a9afc714d2d134702452249da7a30e6fd
mohamed.sayed <mohamed.sayed.salah@outlook.com>
2021-09-27 20:49:03 +0200

    add more configuration to webpack, use tailwind css as theming library

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,43 @@
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // for stand alone generated css files
+
+
 module.exports = {
     entry: "./src/index.js",
     mode: "development", // default is production
+    devtool: 'inline-source-map', // to get a reference to the original file that raised an error
     output: {
         filename: "main.[contenthash].js", // contentHash for cash busting
         path: path.resolve(__dirname, "dist")
     },
-    plugins: [new HtmlWebpackPlugin()],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public/index.html"),
+            inject: true
+        }),
+        new MiniCssExtractPlugin()],
     module: {
         rules: [
             {
-                test: /\.scss$/i,
-                use: ["style-loader", // 3 inject to dom
-                      "css-loader",  // 2 convert css into common js
-                      "sass-loader"], // 1 convert to css
+                test: /\.(css|sass|scss)$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
             },
-        ],
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
     }
-}
+};
